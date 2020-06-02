40450cae189c34a18bc50289bef3c30fdd5394c4
jdele <jordanscottdeleon@gmail.com>
2020-06-02 15:45:21 -0700

    added textinputs to listing

--- client/components/Listing.tsx
+++ client/components/Listing.tsx
@@ -1,5 +1,36 @@
 import React, { useState } from 'react'
+import { View, TextInput } from 'react-native'
 
-const Listing = () => {
+export const Listing = () => {
+  const [title, setTitle] = React.useState('Job Title')
+  const [company, setCompany] = React.useState('Company')
+  const [date, setDate] = React.useState('Application Date')
+  const [notes, setNotes] = React.useState('Notes')
+
+  return (
+    <View>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={text => setTitle(text)}
+        value={title}
+      />
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={text => setCompany(text)}
+        value={company}
+      />
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={text => setDate(text)}
+        value={date}
+      />
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={text => setNotes(text)}
+        value={notes}
+      />
+    </View>
+
+  )
 
 }

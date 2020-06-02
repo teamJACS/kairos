3823c1c0f375be0a110c347ac8237625c465b923
Tony <tony@Emilys-MacBook-Pro.local>
2020-06-01 21:27:36 -0700

    initial kairos app design finished

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+      {/* <Text>Hello World</Text> */}
+      <SignIn />
+      {/* <Button onPress={handleSignUp} /> */}
+    </View >
   );
 }
 

0d32e7051f3210a3501b3b012b76653d526c990a
Cary Chan <caryLchan@gmail.com>
2020-06-02 20:25:21 -0700

    Apollo client onboarded onto kairos and first query made as a test

--- client/src/AppTabs.tsx
+++ client/src/AppTabs.tsx
@@ -1,15 +1,32 @@
 import React, { useContext } from 'react'
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AppParamList } from './AppParamList';
-import { Text, Button } from "react-native";
+import { Text, Button, View } from "react-native";
 import { Center } from "../components/Center";
 import { AuthContext } from './AuthProvider'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag';
 
 interface AppTabsProps {
 
 }
 
 const Tabs = createBottomTabNavigator<AppParamList>();
+
+const FIRST_QUERY = gql`
+query {
+  user {
+ 		jobs {
+      company
+      location
+      notes
+    }
+  }
+}
+`
+
+
+
 
 function Home() {
     const { logout } = useContext(AuthContext)
@@ -20,18 +37,25 @@ function Home() {
         </Center>
     );
 }
-function Application() {
-    return (
-        <Center>
-            <Text>Application</Text>
-        </Center>
-    );
-}
 export const AppTabs: React.FC<AppTabsProps> = ({ }) => {
-    return (
-        <Tabs.Navigator>
-            <Tabs.Screen name='Home' component={Home} />
-            <Tabs.Screen name='Application' component={Application} />
-        </Tabs.Navigator>
-    )
+    const { loading, error, data } = useQuery(FIRST_QUERY)
+
+    if (loading) return <Center><Text>Loading...</Text></Center>
+
+    function Application() {
+        return (
+            <Center>
+                <Text>Sieun from {data.user.jobs[0].company} in {data.user.jobs[0].location} says {data.user.jobs[0].notes}</Text>
+            </Center>
+        );
+    }
+    if (!loading && data) {
+        console.log(data)
+        return (
+            <Tabs.Navigator>
+                <Tabs.Screen name='Home' component={Home} />
+                <Tabs.Screen name='Application' component={Application} />
+            </Tabs.Navigator>
+        )
+    }
 }

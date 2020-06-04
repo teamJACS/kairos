705fd9af41af068d8842b4e69ffbcfcaa758b5e5
Tony <tshen815@gmail.com>
2020-06-04 08:53:50 -0700

    added component to show interiew status number

---
+++ client/components/InterviewNumber.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+
+export const InterviewNumber = () => {
+    const [number, setNumber] = useState('4')
+    return (
+        <Text style={styles.number}>
+            {number}
+        </Text>
+    )
+}
+
+const styles = StyleSheet.create({
+    number: {
+        fontSize: 50,
+        left: 15
+    }
+})

a6f4c40b53695ea718d828292b1a6f8f92f1f40c
Cary Chan <caryLchan@gmail.com>
2020-06-04 13:00:16 -0700

    List view redux state updated and app.json file error fix

--- client/components/Listing.tsx
+++ client/components/Listing.tsx
@@ -7,18 +7,11 @@ import { Dropdown } from 'react-native-material-dropdown';
 // import Icon from 'react-native-vector-icons/FontAwesome';
 
 export const Listing = () => {
-<<<<<<< HEAD
   const [title, setTitle] = useState('')
   const [company, setCompany] = useState('')
-  const [date, setDate] = useState("")
+  const [date, setDate] = useState('')
   const [notes, setNotes] = useState('')
-=======
-  const [title, setTitle] = React.useState('')
-  const [company, setCompany] = React.useState('')
-  const [date, setDate] = React.useState("")
-  const [notes, setNotes] = React.useState('')
-  const [status, setStatus] = React.useState('status')
->>>>>>> 1885684a1ba03e61784e70668eafc55ac813db6b
+  const [status, setStatus] = useState('status')
 
   const saveButton = () => {
     return;
